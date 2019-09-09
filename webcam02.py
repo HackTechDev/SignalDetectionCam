6469581c095bfa56bb4dff2e0e2c3f95b1286a00
Le Sanglier des Ardennes <lesanglierdesardennes@gmail.com>
2019-09-09 22:10:15 +0200

    Add boolean

--- webcam02.py
+++ webcam02.py
@@ -7,16 +7,25 @@ for f in glob.iglob("signe/*"):
     img_rgb = cv2.imread('telegraphe/IMG_20190909_192741960.jpg')
     img_gray = cv2.cvtColor(img_rgb, cv2.COLOR_BGR2GRAY)
 
-    print(f)
+    print(f, end='')
     template = cv2.imread(f, 0)
     w, h = template.shape[::-1]
 
     res = cv2.matchTemplate(img_gray,template,cv2.TM_CCOEFF_NORMED)
     threshold = 0.8
     loc = np.where( res >= threshold)
-    
+ 
+    flag = False
     for pt in zip(*loc[::-1]):
+        if pt is not None:
+            flag = True
+            break
         cv2.rectangle(img_rgb, pt, (pt[0] + w, pt[1] + h), (0,255,255), 2)
+
+    print(flag)
+
+    if flag == True:
+        break
 
     cv2.imshow('Detected',img_rgb)
     cv2.waitKey(0)
