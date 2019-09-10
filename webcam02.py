c38850603134f2df820d5ba5359bc2863e06b8a0
Le Sanglier des Ardennes <lesanglierdesardennes@gmail.com>
2019-09-10 21:49:06 +0200

    With video

--- webcam02.py
+++ webcam02.py
@@ -1,31 +1,40 @@
+import numpy as np
 import cv2
 import glob
-import numpy as np
 
 
-for f in glob.iglob("signe/*"):
-    img_rgb = cv2.imread('telegraphe/IMG_20190909_192741960.jpg')
-    img_gray = cv2.cvtColor(img_rgb, cv2.COLOR_BGR2GRAY)
+cap = cv2.VideoCapture(0)
 
-    print(f, end='')
-    template = cv2.imread(f, 0)
-    w, h = template.shape[::-1]
+while True:
+    ret, img_rgb = cap.read()
 
-    res = cv2.matchTemplate(img_gray,template,cv2.TM_CCOEFF_NORMED)
-    threshold = 0.8
-    loc = np.where( res >= threshold)
- 
-    flag = False
-    for pt in zip(*loc[::-1]):
-        if pt is not None:
-            flag = True
+
+    for f in glob.iglob("signe/*"):
+        img_gray = cv2.cvtColor(img_rgb, cv2.COLOR_BGR2GRAY)
+
+        template = cv2.imread(f, 0)
+        w, h = template.shape[::-1]
+
+        res = cv2.matchTemplate(img_gray,template,cv2.TM_CCOEFF_NORMED)
+        threshold = 0.8
+        loc = np.where( res >= threshold)
+
+        flag = False
+        for pt in zip(*loc[::-1]):
+            if pt is not None:
+                flag = True
+                break
+            cv2.rectangle(img_rgb, pt, (pt[0] + w, pt[1] + h), (0,255,255), 2)
+
+        if flag == True:
+            print("Detected")
             break
-        cv2.rectangle(img_rgb, pt, (pt[0] + w, pt[1] + h), (0,255,255), 2)
 
-    print(flag)
+        cv2.imshow('Detected',img_rgb)
 
-    if flag == True:
+    k = cv2.waitKey(30) & 0xff
+    if k == 27:
         break
 
-    cv2.imshow('Detected',img_rgb)
-    cv2.waitKey(0)
+cap.release()
+cv2.destroyAllWindows()
