5219aa56c8980d6811f162dfb2dc24d31ef074f9
Le Sanglier des Ardennes <lesanglierdesardennes@gmail.com>
2019-09-22 17:14:33 +0200

    Update

--- webcam05.py
+++ webcam05.py
@@ -3,20 +3,33 @@
 
 # https://ressources.labomedia.org/detection_de_mouvement_avec_opencv_en_python
 
+
+import numpy as np
 import cv2
+import glob
+
+def nothing(x):
+    pass
+
 
 def motion_detector(cam, flou, seuil_0, seuil_1, area, tempo):
     # Création des fenêtres
-    cv2.namedWindow('Ecart entre frame')
+    cv2.namedWindow('Gap_between_frame')
     cv2.namedWindow('Thresh')
-    cv2.namedWindow('Security Feed')
+    cv2.namedWindow('Security_Feed')
     
+    cv2.createTrackbar("Threshold_tb", "Security_Feed", 1, 10, nothing)
+
     firstFrame = None
     cap = cv2.VideoCapture(cam)
     loop = 1
     counter = 0
     while loop:
         rval, frame = cap.read()
+
+        threshold_value = cv2.getTrackbarPos("Threshold_tb", "Security_Feed")
+
+
         # Si la webcam à une image
         if rval:
             # Conversion en gris
@@ -28,7 +41,7 @@ def motion_detector(cam, flou, seuil_0, seuil_1, area, tempo):
             if firstFrame is None:
                 firstFrame = gray
             else:
-                # Ecart entre les frames
+                # Gap entre les frames
                 delta = cv2.absdiff(firstFrame, gray)
                 # Seuil
                 thresh = cv2.threshold(delta, seuil_0, seuil_1, cv2.THRESH_BINARY)[1]
@@ -44,13 +57,41 @@ def motion_detector(cam, flou, seuil_0, seuil_1, area, tempo):
                         print("Mouvement detection: ", counter)
                         firstFrame = None 
 
+
+                        for f in glob.iglob("signe/*"):
+                            img_gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+                            
+                            template = cv2.imread(f, 0)
+                            w, h = template.shape[::-1]
+
+                            res = cv2.matchTemplate(img_gray,template,cv2.TM_CCOEFF_NORMED)
+
+                            #threshold = threshold_value / 10
+                            threshold = 0.8
+
+                            loc = np.where( res >= threshold)
+
+                            flag = False
+                            for pt in zip(*loc[::-1]):
+                                if pt is not None:
+                                    flag = True
+                                    break
+                                cv2.rectangle(frame, pt, (pt[0] + w, pt[1] + h), (255, 0, 0), 2)
+
+                            if flag == True:
+                                print(f, end='')
+                                print("Detected")
+                                break
+                                            
+
+
                         # Un rectangle incluant la zone
                         (x, y, w, h) = cv2.boundingRect(c)
                         cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
 
-                        cv2.imshow("Ecart entre frame", delta)
+                        cv2.imshow("Gap_between_frame", delta)
                         cv2.imshow("Thresh", thresh)
-                        cv2.imshow("Security Feed", frame)
+                        cv2.imshow("Security_Feed", frame)
 
 
 
